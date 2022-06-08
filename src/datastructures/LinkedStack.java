ecf38b80bd49fee9b87830bb428837b731b14ad4
Onur <onurhizar@tutanota.com>
2022-06-08 23:10:18 +0300

    feat: display stack in reverse order
    Answer of Quiz-2 question

--- src/datastructures/LinkedStack.java
+++ src/datastructures/LinkedStack.java
@@ -41,9 +41,27 @@ public class LinkedStack<T> {
         return removedNode.data;
     }
 
+    public boolean isEmpty() { return length==0; }
+
     public T peek(){ return lastNode.data; }
 
     public int getLength() { return length; }
+
+    // displays the stack in reverse order without changing the order after displaying
+    public void displayReverse(){
+        LinkedStack<T> temp = new LinkedStack<>();
+        System.out.println("\nREVERSE:\n---------------");
+
+        // transfer from main to temp stack
+        while (!isEmpty()) temp.push(pop());
+
+        // transfer from temp to main stack, order remains same
+        while (!temp.isEmpty()) {
+            T current = temp.pop();
+            push(current);
+            System.out.println(current.toString());
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -52,10 +70,13 @@ public class LinkedStack<T> {
         papers.push("Alice's Paper");
         papers.push("Brooks's Paper");
         papers.push("Carol's Paper");
+        papers.push("David's Paper");
 
         System.out.println("Length: "+papers.getLength());
         System.out.println(papers.pop());
         System.out.println(papers.pop());
         System.out.println("Length: "+papers.getLength());
+
+        papers.displayReverse();
     }
 }
